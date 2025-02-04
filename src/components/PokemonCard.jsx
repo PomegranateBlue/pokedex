0a3ea25cd04e69ed4b535268e80c3a8aebff321f
bluepome98 <wpdn1998@gmail.com>
2025-02-04 14:50:00 +0900

    포켓몬 데이터만 렌더링 -> 스타일 컴포넌트 적용 후에 바뀔 수 있음

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -12,28 +12,28 @@ const PokemonCardFrame = styled.div`
   justify-content: center;
   align-items: center;
 `;
-const PokemonImage = () => styled.img`
-  width: 100px;
-  height: auto;
-`;
+
+const PokemonCardContent = () => styled.div``;
 
 const PokemonCard = () => {
-  const [pokemonData, setPokemonData] = useState({
-    img_url:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-    korean_name: "이상해씨",
-    types: ["풀", "독"],
-    id: 1,
-    description: "풀과 독 타입의 포켓몬으로, 귀여운 식물 모양을 하고 있습니다.",
-  });
+  const [pokemonData, setPokemonData] = useState(POKEMON_DATA);
+  console.log(pokemonData);
   return (
-    <PokemonCardFrame className="card-container">
-      <PokemonImage src={PokemonCard.img_url} />
-      <div className="pokemon-name">{pokemonData.korean_name}</div>
-      <div className="pokemon-types">{pokemonData.types}</div>
-      <div className="pokemon-description">{pokemonData.description}</div>
-      <button>추가</button>
-    </PokemonCardFrame>
+    <div>
+      {pokemonData.map((pokemonInfo) => (
+        <PokemonCardFrame key={pokemonInfo.id}>
+          {" "}
+          <div>
+            <img src={pokemonInfo.img_url}></img>
+          </div>
+          <div>{pokemonInfo.korean_name}</div>
+          <div>{pokemonInfo.types}</div>
+          <div>{pokemonInfo.description}</div>
+          <div>No. {pokemonInfo.id}</div>
+          <button>추가</button>
+        </PokemonCardFrame>
+      ))}
+    </div>
   );
 };
 export default PokemonCard;
