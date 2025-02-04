e015cdcd02aa76415c0b16aeeb9869561ad29ed4
bluepome98 <wpdn1998@gmail.com>
2025-02-04 20:58:34 +0900

    png 파일 로드 시 이미지 깨짐 -> 레이아웃만 잡기

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,13 +2,41 @@ import { useState } from "react";
 import styled from "styled-components";
 
 const SelectedPokemon = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
   background-color: pink;
+`;
+
+const PokeBallSpriteContainer = styled.div`
+  display: flex;
+  margin: 0 auto;
+  justify-content: ;
+`;
+const PokeBallSprite = styled.div`
+  display: flex;
+
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+  background-color: red;
+  justify-content: space-around;
 `;
 const DashBoard = () => {
   const [pokemonInBall, setPokemonInBall] = useState([]);
   return (
     <div>
-      <SelectedPokemon />
+      <SelectedPokemon>
+        <h2>좋아하는 포켓몬은?</h2>
+        <PokeBallSpriteContainer>
+          <PokeBallSprite>1</PokeBallSprite>
+          <PokeBallSprite>2</PokeBallSprite>
+          <PokeBallSprite>3</PokeBallSprite>
+          <PokeBallSprite>4</PokeBallSprite>
+          <PokeBallSprite>5</PokeBallSprite>
+          <PokeBallSprite>6</PokeBallSprite>
+        </PokeBallSpriteContainer>
+      </SelectedPokemon>
     </div>
   );
 };
